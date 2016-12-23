06d5cad02b919489c7440f23be282533ea823fc6
Tushar Tyagi <tushar4r@gmail.com>
2016-12-23 16:40:30 +0530

    Pangram

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -5,7 +5,11 @@ public class Pangram
 {
     private static char[] _alphabets = "abcdefghijklmnopqrstuvwxyz".ToCharArray(); 
 
-    public static bool IsPangram(string input)
+    public static bool IsPangram(string input) {
+        return Panagram2(input);
+    }
+
+    public static bool Panagram1(string input)
     {
         var set = new HashSet<char>();
         foreach (char c in input) 
@@ -18,5 +22,11 @@ public class Pangram
         }
         if (set.Count == 26) return true;
         else return false;
+    }
+
+    public static bool Panagram2(string input) 
+    {
+        var lowercase = input.ToLower();
+        return _alphabets.All(lowercase.Contains);
     }
 }
