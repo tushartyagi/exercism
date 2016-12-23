47e15acebc30469b1107cc6489eacfcc5c585269
Tushar Tyagi <tushar4r@gmail.com>
2016-12-23 16:02:42 +0530

    Bigint to ulong

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace ConsoleApplication
 {
@@ -6,7 +7,8 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var d  = Hamming.Compute("ACT","GGA");
+            
+            
         }
     }
 }
